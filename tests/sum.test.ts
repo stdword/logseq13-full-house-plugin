6d8d498ff28acf12afd5231af94c3b9a8596d79c
Sergey Kolesnik <sergey.s.kolesnik@gmail.com>
2023-05-13 01:31:28 +0300

    test: setup tests

---
+++ tests/sum.test.ts
@@ -0,0 +1,9 @@
+import { f } from '../src/utils/other'
+
+
+test('adds 1 + 2 to equal 3', () => {
+    const format = f`Hello, ${'name'}!`
+    expect(
+        format({name: 'Logseq'})
+    ).toBe('Hello, Logseq!')
+})
