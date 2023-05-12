802425e4bf1133884b851601b4dff0c9bab60465
Sergey Kolesnik <sergey.s.kolesnik@gmail.com>
2023-05-13 02:11:34 +0300

    test: folder reorganisation

--- tests/sum.test.ts
+++
@@ -1,9 +0,0 @@
-import { f } from '../src/utils/other'
-
-
-test('adds 1 + 2 to equal 3', () => {
-    const format = f`Hello, ${'name'}!`
-    expect(
-        format({name: 'Logseq'})
-    ).toBe('Hello, Logseq!')
-})
