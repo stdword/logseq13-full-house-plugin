b55747b1f3ace1759523eac579ba083bc372e1fc
Sergey Kolesnik <sergey.s.kolesnik@gmail.com>
2023-07-26 04:02:00 +0300

    test: template rendering command tests & supporting logseq.api in tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,13 @@
 import 'global-jsdom/register'
 
+import { v4 as genUUID } from 'uuid'
+
+
 import { _private as app } from '@src/app'
+import { BlockEntity, IEntityID, PageEntity } from '@logseq/libs/dist/LSPlugin'
+import { IBlockNode } from '@src/utils'
+
+import { logseq as packageInfo, version as pluginVersion } from '@src/../package.json'
 
 
 export async function LogseqMock(
@@ -11,15 +18,48 @@ export async function LogseqMock(
     configOverride ??= {}
 
     const logseq = {
+        _pages: [] as PageEntity[],
+        _blocks: [] as BlockEntity[],
+
+        baseInfo: {
+            id: packageInfo.id,
+            version: pluginVersion,
+        },
+
         settings: {},
         updateSettings: jest.fn(),
         App: {
+            async getCurrentGraphConfigs() { return {} },
+            async getCurrentGraph() { return {} },
             async getUserConfigs() {
                 const defaultConfig = {
                     preferredDateFormat: 'dd-MM-yyyy',
                 }
                 return Object.assign(defaultConfig, configOverride)
             }
+        },
+        DB: {
+            datascriptQuery: jest.fn(),
+        },
+        Editor: {
+            async getPage(id: number | string, options?: object): Promise<PageEntity | null> {
+                if (typeof id === 'string')
+                    return logseq._pages.find(page => (page.name === id)) ?? null
+                return logseq._pages.find(page => (page.id === id)) ?? null
+            },
+            async getBlock(id: number | string, options?: object): Promise<BlockEntity | null> {
+                if (typeof id === 'string')
+                    return logseq._blocks.find(block => (block.uuid === id)) ?? null
+                return logseq._blocks.find(block => (block.id === id)) ?? null
+            },
+            async insertAtEditingCursor(content: string) {
+                logseq._createBlock({content, children: []})
+            },
+            async updateBlock(uuid, newContent) {
+                const block = await this.getBlock(uuid)
+                if (block)
+                    block.content = newContent
+            },
         },
         UI: {
             showMsg: jest.fn(),
@@ -28,7 +68,57 @@ export async function LogseqMock(
         api: {
             get_block: jest.fn(),
             get_page: jest.fn(),
+            get_app_info: () => { return {} },
         },
+
+        _createPage: function (name: string): PageEntity {
+            const obj: PageEntity = {
+                format: 'markdown',
+
+                id: this._pages.length + 1,
+                uuid: genUUID(),
+
+                name: name,
+                originalName: name,
+
+                'journal?': false,
+                journalDay: undefined,
+            }
+
+            logseq._pages.push(obj)
+            return obj
+        },
+        _createBlock: function (block: IBlockNode | string, parent: BlockEntity | null = null): BlockEntity {
+            const content = typeof block === 'string' ? block : block.content
+            const children = typeof block === 'string' ? [] : block.children
+
+            const obj: BlockEntity = {
+                format: 'markdown',
+
+                id: this._blocks.length + 1,
+                uuid: genUUID(),
+                content: content,
+                properties: {},
+
+                // @ts-expect-error
+                left: {},
+                // @ts-expect-error
+                parent: parent ? {id: parent.id} : {},
+                page: {id: logseq._pages[0].id},
+            }
+            logseq._blocks.push(obj)
+
+            obj.children = children.map((b) => this._createBlock(b, obj))
+            return obj
+        },
+        _createTemplateBlock: function (name: string, content: string) {
+            const obj = logseq._createBlock({
+                content: `template:: ${name}`,
+                children: [{content: content, children: []}],
+            })
+            obj.properties!.template = name
+            return obj
+        }
     }
 
     Object.assign(logseq.settings, settingsOverride ?? {})
@@ -36,5 +126,7 @@ export async function LogseqMock(
     // @ts-expect-error
     global.logseq = top.logseq = logseq
     await app.init()
+
+    logseq._createPage('PAGE')
     return logseq
  }
---
+++ tests/logic/command_template.test.ts
@@ -0,0 +1,70 @@
+import { v4 as uuid } from 'uuid'
+
+import { LogseqMock } from '@tests/index'
+
+import { _private as tags } from '@src/tags'
+import { _private as app } from '@src/app'
+import { RendererMacro, parseReference } from '@src/utils'
+
+
+import { renderTemplateInBlock } from '@src/logic'
+
+
+async function testRender(syntax: string, expected: string | null = null) {
+    const logseq = await LogseqMock()
+
+    const name = 'test_name'
+    const command = RendererMacro.command('template').arg(name)
+    const block = logseq._createBlock(command.toString())
+    if (expected === '')
+        expected = block.content
+
+    const templateBlock = logseq._createTemplateBlock(name, syntax)
+
+    logseq.DB.datascriptQuery.mockReturnValue([templateBlock])
+    await renderTemplateInBlock('slot__test', block.uuid, parseReference(name), command, [])
+
+    expect(logseq.DB.datascriptQuery).toHaveBeenCalledTimes(1)
+    if (expected !== null)
+        expect(block.content).toBe(expected)
+
+    return block.content
+ }
+
+describe.only('template syntax', () => {
+    test('no syntax', async () => {
+        await testRender('text', 'text') })
+    test('js expression', async () => {
+        await testRender('``1 + 2``', '3') })
+    test('js statement', async () => {
+        await testRender('``{ console._checkMe = 13 }``', '')
+        // @ts-expect-error
+        expect(console._checkMe).toBe(13)
+    })
+
+    test('save spaces to the right', async () => {
+        await testRender('``{ }``  text', '  text') })
+    test('save spaces to the left', async () => {
+        await testRender('text  ``{ }``', 'text  ') })
+    test('strip one new line to the right', async () => {
+        await testRender('``{ }``\n\ntext', '\ntext') })
+    test('strip no one new lines to the left', async () => {
+        await testRender('text\n\n``{ }``', 'text\n\n') })
+
+    test('ref shortcut', async () => {
+        await testRender('``["page"]``', '[[page]]') })
+})
+
+describe.only('template context', () => {
+    test('page name', async () => {
+        await testRender('``c.page.name``', 'PAGE') })
+    test('template name', async () => {
+        await testRender('``c.template.name``', 'test_name') })
+    test('identity', async () => {
+        await testRender('``c.identity.slot``, ``c.identity.key``', 'slot__test, test') })
+    test('full context', async () => {
+        const content = await testRender('``c``')
+        expect(content.slice(0, 9)).toBe('```json\n{')
+        expect(content.slice(-5)).toBe('}\n```')
+    })
+})
