68c7d9a02f829f979d232433cd4ea17b052f4e11
Sergey Kolesnik <sergey.s.kolesnik@gmail.com>
2023-11-05 07:51:34 +0300

    feat(ui): open template block with shift

--- src/ui/insert.tsx
+++ src/ui/insert.tsx
@@ -45,6 +45,10 @@ async function collectTemplatesList(): Promise<Data> {
     })
 }
 
+async function openLogic(item: DataItem) {
+    logseq.App.pushState('page', {name: item.uuid})
+}
+
 async function insertLogic(
     item: DataItem,
     blockUUID: string,
@@ -124,6 +128,7 @@ function InsertUI({ blockUUID, isSelectedState }) {
     const [highlightedWithMouse, setHighlightedWithMouse] = useState(false)
     const [optKeyPressed, setOptKeyPressed] = useState(false)
     const [cmdKeyPressed, setCmdKeyPressed] = useState(false)
+    const [shiftKeyPressed, setShiftKeyPressed] = useState(false)
 
     const data = useRef([] as Data)
     async function prepareData() {
@@ -161,6 +166,7 @@ function InsertUI({ blockUUID, isSelectedState }) {
         // in case of loosing input focus with pressed Opt/Meta keys: KeyUp event didn't fire
         setOptKeyPressed(false)
         setCmdKeyPressed(false)
+        setShiftKeyPressed(false)
     }
 
     useEffect(() => {
@@ -201,6 +207,9 @@ function InsertUI({ blockUUID, isSelectedState }) {
         if (event.key === 'Alt') {
             setOptKeyPressed(true)
         }
+        else if (event.key === 'Shift') {
+            setShiftKeyPressed(true)
+        }
         else if ((isMacOS && event.key === 'Meta') || (!isMacOS && event.key === 'Control')) {
             setCmdKeyPressed(true)
         }
@@ -238,6 +247,9 @@ function InsertUI({ blockUUID, isSelectedState }) {
     const handleEscapeKey = (event: KeyboardEvent) => {
         if (event.key === 'Alt') {
             setOptKeyPressed(false)
+        }
+        else if (event.key === 'Shift') {
+            setShiftKeyPressed(false)
         }
         else if ((isMacOS && event.key === 'Meta') || (!isMacOS && event.key === 'Control')) {
             setCmdKeyPressed(false)
@@ -311,11 +323,24 @@ function InsertUI({ blockUUID, isSelectedState }) {
             scrollToHightlightedItem()
     }, [highlightedIndex])
 
+    useEffect(() => {
+        const itemsElement = document.getElementById('items')! as HTMLElement
+        itemsElement.childNodes.forEach((node) => {
+            const item = node as HTMLElement
+            if (!shiftKeyPressed)
+                item.classList.remove('will-open')
+            else if (item.classList.contains('selected'))
+                item.classList.add('will-open')
+        })
+    }, [shiftKeyPressed])
+
     const highlightItem = (event: MouseEvent) => {
         const currentItem = (event.target! as HTMLDivElement).closest('.item')
         const itemsElement = document.getElementById('items')!
         for (const [index, node] of Object.entries(itemsElement.childNodes)) {
             if (node === currentItem) {
+                if (shiftKeyPressed)
+                    (node as HTMLDivElement).classList.add('will-open')
                 setHighlightedWithMouse(true)
                 setHighlightedIndex(Number(index))
                 break
@@ -329,8 +354,15 @@ function InsertUI({ blockUUID, isSelectedState }) {
 
         hideUI()
 
+        const chosenItem = results[highlightedIndex]
+
+        if (shiftKeyPressed) {
+            openLogic(chosenItem)
+            return
+        }
+
         await insertLogic(
-            results[highlightedIndex],
+            chosenItem,
             blockUUID,
             isSelectedState,
             cmdKeyPressed ? 'View' : 'Template',
@@ -391,20 +423,25 @@ function InsertUI({ blockUUID, isSelectedState }) {
                         <footer>
                             <ul>
                                 <li>
-                                    <svg role="graphics-symbol" viewBox="0 0 16 16" class="arrowsUpDown">
+                                    <svg role="img" viewBox="0 0 16 16" class="arrowsUpDown">
                                         <path d="M4.11 2.385a.61.61 0 01.48-.211c.191 0 .353.07.486.21L8.03 5.409a.66.66 0 01.2.475c0 .191-.061.347-.182.469a.627.627 0 01-.463.181.59.59 0 01-.451-.187L5.926 5.098l-.72-.844.04 1.219v7.242a.624.624 0 01-.187.469.624.624 0 01-.47.187.636.636 0 01-.468-.187.635.635 0 01-.182-.47V5.474l.041-1.22-.726.845-1.201 1.248a.61.61 0 01-.457.187.627.627 0 01-.463-.181.634.634 0 01-.182-.47.66.66 0 01.2-.474l2.958-3.023zm7.786 10.781a.636.636 0 01-.486.205.665.665 0 01-.486-.205l-2.947-3.035a.64.64 0 01-.206-.475c0-.191.061-.345.182-.463a.634.634 0 01.469-.181c.18 0 .33.06.451.181l1.201 1.248.727.85-.041-1.219V2.824c0-.187.06-.342.181-.463a.636.636 0 01.47-.187.627.627 0 01.65.65v7.248l-.041 1.219.726-.85 1.207-1.248a.6.6 0 01.451-.181c.188 0 .342.06.463.181a.615.615 0 01.182.463.648.648 0 01-.2.475l-2.953 3.035z"></path>
                                     </svg>Select</li>
                                 <li>
                                     <span>{ isMacOS ? '⌥' : 'alt' } </span>Show hidden</li>
                                 <li>
-                                    <svg role="graphics-symbol" viewBox="0 0 16 16" class="enter">
+                                    <svg role="img" viewBox="0 0 16 16" class="enter">
                                         <path d="M5.38965 14.1667C5.81812 14.1667 6.10156 13.8767 6.10156 13.468C6.10156 13.2571 6.01587 13.0989 5.89062 12.967L4.18994 11.3125L3.02979 10.3369L4.55908 10.4028H12.7922C14.4402 10.4028 15.1389 9.65796 15.1389 8.04297V4.13403C15.1389 2.48608 14.4402 1.78735 12.7922 1.78735H9.13379C8.70532 1.78735 8.4021 2.11035 8.4021 2.50586C8.4021 2.90137 8.69873 3.22437 9.13379 3.22437H12.7593C13.4316 3.22437 13.7151 3.50781 13.7151 4.17358V7.99683C13.7151 8.67578 13.425 8.95923 12.7593 8.95923H4.55908L3.02979 9.03174L4.18994 8.04956L5.89062 6.39502C6.01587 6.26978 6.10156 6.11157 6.10156 5.89404C6.10156 5.48535 5.81812 5.19531 5.38965 5.19531C5.21167 5.19531 5.01392 5.27441 4.8689 5.41943L1.08521 9.1438C0.933594 9.28882 0.854492 9.48657 0.854492 9.68433C0.854492 9.87549 0.933594 10.0732 1.08521 10.2183L4.8689 13.9492C5.01392 14.0876 5.21167 14.1667 5.38965 14.1667Z"></path>
                                     </svg>Insert as Template</li>
                                 <li>
                                     <span>{ isMacOS ? '⌘' : 'ctrl' }</span>
-                                    <svg role="graphics-symbol" viewBox="0 0 16 16" class="enter">
+                                    <svg role="img" viewBox="0 0 16 16" class="enter">
                                         <path d="M5.38965 14.1667C5.81812 14.1667 6.10156 13.8767 6.10156 13.468C6.10156 13.2571 6.01587 13.0989 5.89062 12.967L4.18994 11.3125L3.02979 10.3369L4.55908 10.4028H12.7922C14.4402 10.4028 15.1389 9.65796 15.1389 8.04297V4.13403C15.1389 2.48608 14.4402 1.78735 12.7922 1.78735H9.13379C8.70532 1.78735 8.4021 2.11035 8.4021 2.50586C8.4021 2.90137 8.69873 3.22437 9.13379 3.22437H12.7593C13.4316 3.22437 13.7151 3.50781 13.7151 4.17358V7.99683C13.7151 8.67578 13.425 8.95923 12.7593 8.95923H4.55908L3.02979 9.03174L4.18994 8.04956L5.89062 6.39502C6.01587 6.26978 6.10156 6.11157 6.10156 5.89404C6.10156 5.48535 5.81812 5.19531 5.38965 5.19531C5.21167 5.19531 5.01392 5.27441 4.8689 5.41943L1.08521 9.1438C0.933594 9.28882 0.854492 9.48657 0.854492 9.68433C0.854492 9.87549 0.933594 10.0732 1.08521 10.2183L4.8689 13.9492C5.01392 14.0876 5.21167 14.1667 5.38965 14.1667Z"></path>
                                     </svg>Insert as View</li>
+                                <li>
+                                    <span>{ isMacOS ? '⇧' : 'shift' }</span>
+                                    <svg role="img" viewBox="0 0 16 16" class="enter">
+                                        <path d="M5.38965 14.1667C5.81812 14.1667 6.10156 13.8767 6.10156 13.468C6.10156 13.2571 6.01587 13.0989 5.89062 12.967L4.18994 11.3125L3.02979 10.3369L4.55908 10.4028H12.7922C14.4402 10.4028 15.1389 9.65796 15.1389 8.04297V4.13403C15.1389 2.48608 14.4402 1.78735 12.7922 1.78735H9.13379C8.70532 1.78735 8.4021 2.11035 8.4021 2.50586C8.4021 2.90137 8.69873 3.22437 9.13379 3.22437H12.7593C13.4316 3.22437 13.7151 3.50781 13.7151 4.17358V7.99683C13.7151 8.67578 13.425 8.95923 12.7593 8.95923H4.55908L3.02979 9.03174L4.18994 8.04956L5.89062 6.39502C6.01587 6.26978 6.10156 6.11157 6.10156 5.89404C6.10156 5.48535 5.81812 5.19531 5.38965 5.19531C5.21167 5.19531 5.01392 5.27441 4.8689 5.41943L1.08521 9.1438C0.933594 9.28882 0.854492 9.48657 0.854492 9.68433C0.854492 9.87549 0.933594 10.0732 1.08521 10.2183L4.8689 13.9492C5.01392 14.0876 5.21167 14.1667 5.38965 14.1667Z"></path>
+                                    </svg>Go to block</li>
                             </ul>
                         </footer>
                     </div>
